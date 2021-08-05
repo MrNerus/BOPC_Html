8ef561f5b27e72871e35628c033b3a815ea77682
unknown <mr.ard.nerus@gmail.com>
2021-08-05 16:00:46 +0545

    Some Tweaks

---
+++ challange 1 - Joker/execute.js
@@ -0,0 +1,116 @@
+Help_Toggle = 0;
+var Setup = "****";
+var Punchline = "***";
+var Url = 'https://official-joke-api.appspot.com/random_joke';
+var captured;
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+Set_Joke();
+
+// =====================
+
+
+function Click_Info() {
+    if (Help_Toggle == 0) {
+        Show_Info();
+    }
+    else if (Help_Toggle == 1) {
+        Hide_Info();
+    }
+}
+
+function Show_Info() {
+    Help_Toggle = 1;
+    console.log(Help_Toggle);
+    document.getElementById("Help_Box").style.height = "35px";
+}
+
+function Hide_Info() {
+    Help_Toggle = 0;
+    document.getElementById("Help_Box").style.height = "0px";
+}
+
+
+// ====================
+
+// function Set_Joke(data) {
+
+//     console.log(data);
+//     captured = data;
+//     console.log(captured);
+
+//     Setup = captured["setup"];
+//     Punchline = captured["punchline"];
+//     console.log(Setup);
+//     console.log(Punchline);
+//     console.log("Exec fetching");
+//     return true;
+// }
+
+
+// function Another_One() {
+//     document.getElementById("Buffer").style.height = "30px";
+
+//     let Url = 'https://official-joke-api.appspot.com/random_joke';
+//     try {
+//         fetch(Url)
+//         .then(function(response) {
+//             return response.json();
+//         })
+//         .then(function(data) {
+//             Set_Joke(data);
+//         })
+//         .catch(function(err) {
+//             console.log('error: ' + err);
+//         });
+//         wait(4*1000);
+
+//     } catch (error) {
+//         console.log(`error: ${error}`);
+//     }
+//     console.log(Setup);
+//     console.log(Punchline);
+//     Update_Setup();
+//     document.getElementById("Buffer").style.height = "0px";
+// }
+
+function initialize() {
+    document.getElementById("Buffer").style.height = "30px";
+
+
+    return new Promise(function (resolve, reject) {
+        fetch(Url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                captured = data;
+                Setup = captured["setup"];
+                Punchline = captured["punchline"];
+                resolve();
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+                reject();
+            });
+    })
+}
+
+function Set_Joke(){
+    initialize().then(function () {
+        Update_Setup();
+        document.getElementById("Buffer").style.height = "0px";
+    })
+    .catch(function (errorx) {
+        console.log('error: ' + errorx);
+    });
+}
+
+
+
+function Update_Setup() {
+    document.getElementById("Setup").innerHTML = Setup;
+    document.getElementById("Punchline").innerHTML = "...";
+}
+function Update_Punchine() {
+    document.getElementById("Punchline").innerHTML = Punchline;
+}
